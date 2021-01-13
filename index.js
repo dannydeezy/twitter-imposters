8cd17e5fa7ba8591af71184a311e94e42c898e9b
Danny Diekroeger <dannydiek@gmail.com>
2021-01-12 19:03:54 -0800

    Log event in aws lambda

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const find = require('./find')
 
 // Used if rurnning this as an AWS Lambda function.
 exports.handler = async (event) => {
+    console.log(JSON.stringify(event))
     return find.start(event.username, event.queries, true, event.report)
 }
