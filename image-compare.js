2a7fd05bf22a709898f165f17e7cb03d36c9ce0d
Danny Diekroeger <dannydiek@gmail.com>
2021-01-12 21:36:34 -0800

    Improve image recognition

--- image-compare.js
+++ image-compare.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
-const child_process = require('child_process')
 const request = require('request-promise')
+const { imgDiff } = require("img-diff-js");
 const imageFolder = `/tmp/twitter-imposter-img`
-let originalImage
+let originalImage, originalImageLocation
 
 if (!fs.existsSync(imageFolder)) {
     fs.mkdirSync(imageFolder)
@@ -15,13 +15,24 @@ function imageType(url) {
 
 async function fetchAndSaveOriginalProfilePic(url) {
     originalImageLocation = `${imageFolder}/original.${imageType(url)}`
-    originalImage = await request(url)
+    originalImage = await request({ url, encoding: null })
+    fs.writeFileSync(originalImageLocation, Buffer.from(originalImage, 'utf8'))
 }
 
-async function hasSameProfilePic(userObj, callback) {
+async function hasSameProfilePic(userObj) {
     const url = userObj.profile_image_url_https
-    const imposterImage = await request(url)
-    return imposterImage === originalImage
+    const imposterImage = await request({ url, encoding: null })
+    const imageLocation = `${imageFolder}/${userObj.screen_name}.${imageType(url)}`
+    fs.writeFileSync(imageLocation, Buffer.from(imposterImage, 'utf8'))
+    return new Promise((resolve, reject) => {
+        imgDiff({	
+            actualFilename: originalImageLocation,	
+            expectedFilename: imageLocation,
+            options: {
+                threshold: 0.7
+            }
+        }).then(result => { resolve(result.imagesAreSame)})
+    })
 }
 
 module.exports = { fetchAndSaveOriginalProfilePic, hasSameProfilePic }
